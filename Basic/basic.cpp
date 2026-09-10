f6a5a9177468a8f96b8c72fc852396978ceff1af
Saurav Kaushik <sauravkaushik597@gmail.com>
2026-09-10 21:07:24 +0530

    Make basic file structure with test file and set git repo

---
+++ Basic/basic.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    cout<<"Yooo\n"<<"What's ur name boii??\n";
+    cin>>name;
+    cout<<"Nice to see ya here, "<<name<<":)\n";
+    return 0;
+}
