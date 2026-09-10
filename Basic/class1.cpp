fd12dc073cd65e09438e75b6c6c01ce257d82498
Saurav Kaushik <sauravkaushik597@gmail.com>
2026-09-10 22:59:06 +0530

    Revised Bitwise operators and Data Type Modifiers with problems

--- Basic/class1.cpp
+++ Basic/class1.cpp
@@ -1,5 +1,22 @@
-#include <iostream>;
+#include <iostream>
 using namespace std;
 int main(){
-    cout<<"Is it working ??";
+    int a =6, b = 10;
+    cout<<"Is it working ??"<<endl;
+
+    //bitwise operators check
+    cout<<(a & b)<<endl ; //bitwise and git
+    cout<<(a | b)<<endl ; //bitwise OR
+    cout<<(a ^ b)<<endl ; //bitwise XOR
+
+    int n = 8, i = 1;
+    cout<<(n << i)<<endl ; //left shift <<, a*2powerb
+    cout<<(n >> i)<<endl ; //right shift >> a/2powerb
+
+    //data type modifiers
+    cout<<sizeof(int)<<"bytes"<<endl;
+    cout<<sizeof(long int)<<"bytes"<<endl;
+    cout<<sizeof(short int)<<"bytes"<<endl;
+    cout<<sizeof(long long int)<<"bytes"<<endl;
+    cout<<sizeof(unsigned int)<<"bytes"<<endl;
 }
