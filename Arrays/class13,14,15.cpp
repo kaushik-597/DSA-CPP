06ab9a377bc2e903f24908b2cd2e894bed0fc03a
Saurav Kaushik <sauravkaushik597@gmail.com>
2026-09-23 17:16:59 +0530

    Solve the single number in sorted array problem using binary search

--- Arrays/class13,14,15.cpp
+++ Arrays/class13,14,15.cpp
@@ -31,9 +31,51 @@ int peakMount(vector<int> mountArr) {
     return -1; // this return statement is unnecessary cuz the mount array will always have a peak
 }
 
+// single number in sorted array - where every element repeats twice but there's a single number among them
+// using binary search - O(log n)
+int singleNum(vector<int> numArr) {
+    int n = numArr.size();
+    // first check if the array has size > 1
+    if (n == 1)
+        return numArr[0];
+
+    // the binary search loop
+    int start = 0, end = n - 1;
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        // edge case checks
+        if (mid == 0 && numArr[0] != numArr[1])
+            return numArr[mid];
+        if (mid == n - 1 && numArr[n - 1] != numArr[n - 2])
+            return numArr[mid];
+
+        // now to check if the mid is single number or not
+        if (numArr[mid - 1] != numArr[mid] && numArr[mid] != numArr[mid + 1])
+            return numArr[mid];
+
+        // check if the left and right halves are even or odd
+        if (mid % 2 == 0) { // even
+            if (numArr[mid - 1] == numArr[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        } else {
+            if (numArr[mid - 1] == numArr[mid])
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+    }
+    return -1; // in case input is not correct i.e. no single number ever existed
+}
+
 int main() {
     vector<int> mountainArr = {0, 3, 8, 9, 5, 2};
+    vector<int> numArr = {1, 1, 2, 3, 3, 4, 4};
 
     // results of peak idx of mount array
     cout << "the peak element is at arr[" << peakMount(mountainArr) << "]" << endl;
+
+    // results of single number in sorted array
+    cout << "the single number in sorted array is - " << singleNum(numArr) << endl;
 }
